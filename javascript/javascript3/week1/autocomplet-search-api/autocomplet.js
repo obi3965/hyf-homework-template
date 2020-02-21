90fb616774970047bf2b5b6d5426f87ddd4f5181
obaidullah3965 <33298720+obaidullah3965@users.noreply.github.com>
2020-02-21 23:56:11 +0100

    my autocomplet search is on progress

---
+++ javascript/javascript3/week1/autocomplet-search-api/autocomplet.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+
+let search = document.querySelector('#search');
+let matchList = document.querySelector('#match-list')
+
+
+//SEARCH AUTOCOMPLET.JSON AND FILTER IT
+let searchStates = async searchText =>{
+    let res = await fetch('./autocomplet.json')
+    let states = await resizeBy.json();
+
+    //GET MATCH TO CURRENT TEXT INPUT
+    let matches = states.filter(state =>{
+        let regex = new RexExp(`^${searchText}`, 'gi');
+        return state.name.match(regex) || state.abbr.match(regex);
+    })
+
+    if(searchText.length === 0){
+        matches =[];
+    }
+
+    outputHtml(matches);
+
+    // console.log(states)
+}
+
+let outputHtml = matches =>{
+    if(matches.length > 0){
+        let html = matches.map(match => `
+        
+         <div class="card card-body mb-1">
+           <h4>${match.name}(${match.abbr}) <span class="text-primary">${match.capital}</span></h4>
+         </div>
+        `
+        )
+    }
+}
+//WE CAN LISTEN FOR THE INPUT BECAUSE IT CAN BE ANY INPUTES.WE CAN ALSO USE THE KEYuP
+
+search.addEventListener('input', ()=> searchStates(search.value))
+})
